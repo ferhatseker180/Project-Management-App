d563974e7dcee7048d4fe8e3bbf6b57ed8cb0fba
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2025-01-28 20:13:48 +0300

    Fixed the problem of not being able to add more than one task to the same project

--- SpringBoot/ProjectManagementApp/src/main/java/org/ferhat/project_management_app/dto/response/task/TaskResponse.java
+++ SpringBoot/ProjectManagementApp/src/main/java/org/ferhat/project_management_app/dto/response/task/TaskResponse.java
@@ -3,7 +3,6 @@ package org.ferhat.project_management_app.dto.response.task;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.ferhat.project_management_app.dto.response.project.ProjectResponse;
 
--- SpringBoot/ProjectManagementApp/src/main/java/org/ferhat/project_management_app/entities/Project.java
+++ SpringBoot/ProjectManagementApp/src/main/java/org/ferhat/project_management_app/entities/Project.java
@@ -1,10 +1,7 @@
 package org.ferhat.project_management_app.entities;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
